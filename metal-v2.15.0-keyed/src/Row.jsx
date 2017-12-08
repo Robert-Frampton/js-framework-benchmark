89249401bb3a32dcf1178f2baf4fe45652b2c70c
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2017-12-08 12:41:09 -0800

    Provide both keyed and non-keyed metal tests (match react 16 tests where non-keyed tests do not have as many child components)

--- metal-v2.15.0-keyed/src/Row.jsx
+++ metal-v2.15.0-keyed/src/Row.jsx
@@ -85,3 +85,7 @@ export class Row extends Component {
 	}
 }
 
+Row.PROPS = {
+	data: {},
+	styleClass: {}
+};
---
+++ metal-v2.15.0-non-keyed/src/Row.jsx
@@ -0,0 +1,38 @@
+'use strict';
+
+import Component, {Config} from 'metal-jsx';
+
+export class Row extends Component {
+	created() {
+		this.onDelete = this.onDelete.bind(this);
+		this.onClick = this.onClick.bind(this);
+	}
+	shouldUpdate(stateChanged, propsChanged) {
+		const {data, styleClass} = propsChanged;
+
+		return (data && data.newVal !== data.prevVal) || (styleClass && styleClass.newVal !== styleClass.prevVal);
+	}
+	onDelete() {
+		this.props.onDelete(this.props.data.id);
+	}
+	onClick() {
+		this.props.onClick(this.props.data.id);
+	}
+
+	render() {
+		let {styleClass, onClick, onDelete, data} = this.props;
+		return (<tr class={styleClass}>
+			<td class="col-md-1">{data.id}</td>
+			<td class="col-md-4">
+				<a onClick={this.onClick}>{data.label}</a>
+			</td>
+			<td class="col-md-1"><a onClick={this.onDelete}><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></a></td>
+			<td class="col-md-6"></td>
+		</tr>);
+	}
+}
+
+Row.PROPS = {
+	data: {},
+	styleClass: {}
+};
